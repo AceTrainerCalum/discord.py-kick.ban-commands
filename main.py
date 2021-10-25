e3b087d8542b30c0fcc70cd9631f0ad45e87e1ba
AceTrainerCalum <79257331+AceTrainerCalum@users.noreply.github.com>
2021-10-25 19:23:30 +0100

    adds comments
    added comments to the file and added some data for the defult discord.py help command

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# imports
 import discord
 from discord.ext import commands, tasks
 
+#set the client
 client = commands.Bot(command_prefix="T!")
 
-@client.command()
+@client.command(name="kick", help="kicks a user) #client command
 async def kick(ctx, member = discord.member, *, reason = None):
 
   if reason == None:
@@ -15,7 +17,7 @@ async def kick(ctx, member = discord.member, *, reason = None):
   except:
     await ctx.send("there was a problem")
 
-@client.command()
+@client.command(name="ban", help="bans a user") # client command
 async def ban(ctx, member = discord.member, *, reason = None):
 
   if reason == None:
